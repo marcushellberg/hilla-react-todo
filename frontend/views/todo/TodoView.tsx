0879af5a501d7ae675c3f9cf64c0a6bb408919ce
start.vaadin.com <no-reply@vaadin.com>
2022-11-14 12:44:56 +0200

    Generated project

---
+++ frontend/views/todo/TodoView.tsx
@@ -0,0 +1,14 @@
+import cn from 'classnames';
+import type { ReactElement } from 'react';
+
+export default function TodoView(): ReactElement<unknown> | null {
+  return (
+    <div
+      className={cn('flex', 'flex-col', 'h-full', 'items-center', 'justify-center', 'p-l', 'text-center', 'box-border')}
+    >
+      <img style={{ width: '200px' }} src="images/empty-plant.png" />
+      <h2>This place intentionally left empty</h2>
+      <p>It’s a place where you can grow your own UI 🤗</p>
+    </div>
+  );
+}
