74ca116799f6be6e8928d67d60e19d6898c06b37
Marcus Hellberg <marcus@vaadin.com>
2022-11-21 16:30:22 -0800

    simplify a bit

--- frontend/thirdParty.tsx
+++
@@ -1,8 +0,0 @@
-import { createComponent } from '@lit-labs/react';
-import { NavItem as VcfNavItem } from '@vaadin-component-factory/vcf-nav/src/vcf-nav-item.js';
-import { Nav as VcfNav } from '@vaadin-component-factory/vcf-nav/src/vcf-nav.js';
-import * as React from 'react';
-
-export const Nav = createComponent(React, 'vcf-nav', VcfNav);
-
-export const NavItem = createComponent(React, 'vcf-nav-item', VcfNavItem);
